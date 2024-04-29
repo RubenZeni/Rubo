d1b14e4015b131df2f69e3feb1d6f6b41c590ce1
RubenZeni <zenirubendario@gmail.com>
2024-04-29 17:52:16 -0300

    Retoque de estilo

--- Prácticas.py
+++ Prácticas.py
@@ -1,40 +1,172 @@
+#  Se tiene un listado con los siguientes datos: Número de alumno (1 a n), número de materia (1 a m), nota (0 a 10). * El mismo número de alumno y de materia puede aparecer más de una vez. * El listado no está ordenado, ni necesariamente completo. Esto último quiere decir que puede ser que un alumno no haya cursado una o más materias, y por lo tanto no existan los datos correspondientes en el listado. Se pide: a- Crear una estructura bidimensional que almacene el promedio por materia de cada alumno e informarla asignándole en la impresión un guión en caso de faltar datos. b- Informar el porcentaje de alumnos que cursó cada materia y el promedio general por materia considerando los alumnos que la cursaron. c- Informar la cantidad de materias que cursó cada alumno y el promedio que obtuvo considerando las materias que cursó.
+def Ejercicio6():
+    alumnos = []
+    materias = []
+    notas = []
+    promedios = []
+    alumno_anotado = -1
+    materia_anotada = -1
+    promedio_general = 0
+    alumnos_que_cursaron = 0
+    total_alumnos = 0
+    posicion_ultimo_alumno = 0
+    
+    numero_alumno = int(input('Número de alumno (0 para finalizar): '))
+    while numero_alumno != 0:
+        alumnos.append(numero_alumno)
+        materias.append(int(input('Número de materia: ')))
+        nota_alumno = input('Nota: ')
+        if nota_alumno == '':
+            notas.append('-')
+        else:
+            notas.append(int(nota_alumno))
+        numero_alumno = int(input('Número de alumno (0 para finalizar): '))
+    
+    promedios.append([alumnos[0], materias[0], 0, 0, 1, 0]) # Asigno a la posición 0 de los promedios (con alumno en posición 0): Número de alumno, Número de materia, Nota, Promedio general del alumno, Cantidad de materias del alumno y Contador de notas para calcular el promedio de cada materia del alumno
+    for i in range(len(alumnos)): # Recorro la lista de alumnos (con i)
+        print(f'VALOR I = {i}')
+        for j in (posicion_ultimo_alumno, 0, -1): # Recorro desde la posición actual hacia atrás (con j)
+            print(f'VALOR J = {j}')
+            if alumnos[i] == promedios[j][0]: # ¿Es el alumno en la posición actual (i) igual al que se encuentra en la posición j? (j irá cambiando)
+                alumno_anotado = j # Si lo es, guardo la posición del alumno
+                if materias[i] == promedios[j][1]: # Además ¿Es la materia en la posición actual (la del alumno, i) igual a la que se encuentra en la posición j? (j irá cambiando)
+                    materia_anotada = j # Si lo es, guardo la posición de la materia
+                else:
+                    materia_anotada = -1 # Si no lo es, guardo un número negativo indicando que no hay posición de materia
+                break # Además, freno el bucle en reversa
+            else:
+                alumno_anotado = -1 # Si no lo es, guardo un número negativo indicando que no hay posición de alumno
+                if notas[i] != '-': # ¿El alumno cursó la materia?
+                    alumnos_que_cursaron += 1 # Si es así, sumno 1 al contador para el porcentaje
+        if materia_anotada >= 0: # Una vez finalizado el bucle en reversa, ¿Ya existía el número del alumno en la lista, con la misma materia?
+            if notas[i] != '-': # ¿El alumno cursó la materia?
+                promedios[alumno_anotado][2] += notas[i] # Si es así, entonces sumo la nota a la actual
+                promedios[alumno_anotado][3] += notas[i] # Y también sumo la nota a la de promedio general del alumno
+                promedios[alumno_anotado][5] += 1 # Además, agrego uno al contador de notas para poder calcular el promedio
+        elif alumno_anotado >= 0: # Si no es así, ¿Estaba el número del alumno pero sin la misma materia?
+            promedios.append([alumnos[i], materias[i], notas[i], 0, 1, 0]) # Si es así, asigno un nuevo elemento para el mismo alumno pero con una materia distinta a la lista
+            posicion_ultimo_alumno += 1 # Al haber agregado un elemento a la lista debo actualizar el contador de posiciones
+            if notas[i] != '-': # ¿El alumno cursó la materia?
+                promedios[alumno_anotado][4] += 1 # Si es así, entonces sumo uno a la cantidad de materias que cursó el alumno
+        else: # ¿No estaba ya el número del alumno en la lista?
+            total_alumnos += 1
+            promedios.append([alumnos[i], materias[i], notas[i], 0, 1, 0]) # Si es así, entonces asigno un nuevo elemento para el alumno su respectiva materia y nota
+            posicion_ultimo_alumno += 1 # Al haber agregado un elemento a la lista debo actualizar el contador de posiciones
+    promedios.sort() # Ordeno la lista de promedios para tener a todos los alumnos en orden
+    for i in range(len(promedios)): # Recorro la lista de promedios
+        if promedios[i][2] != '-': # ¿El alumno cursó la materia?
+            promedio_general += promedios[i][2] # Si es así, sumo los promedios de los alumnos que cursaron para tener el promedio general
+            if promedios[i][5] != 0:
+                promedios[i][2] = promedios[i][2] / promedios[i][5] # Además, calculo el promedio de cada alumno
+            del promedios[i][5] # Y elimino el elemento contador de notas porque no me sirve más
+    ver = input('¿Desea visualizar la martiz? (s/n): ')
+    print()
+    if ver == 's':
+        for i in range(len(promedios)):
+            print('[', end = '')
+            for j in range(len(promedios[0])):
+                print(promedios[i][j], end = '')
+                if j != len(promedios[0]) - 1:
+                    print(', ', end = '')
+            print(']')
+        print()
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Pila - Recursividad
+class Stack:
+    def __init__(self):
+        self.__elements = []
+    def push(self, element):
+        self.__elements.append(element)
+    def pop(self):
+        if len(self.__elements) > 0:
+            return self.__elements.pop()
+        else:
+            return None
+    def on_top(self):
+        if len(self.__elements) > 0:
+            return self.__elements[-1]
+        else:
+            return None
+    def size(self):
+        return len(self.__elements)
+
+def llenarPila(n):
+    pila = Stack()
+    pila.push("a")
+    pila.push("b")
+    pila.push("c")
+    pila.push("d")
+    pila.push("e")
+    pila.push("f")
+    pila.push("g")
+    pila.push("h")
+    pila.push("i")
+    pila.push("j")
+    pila.push("k")
+    pila.push("l")
+
+pila_aux = Stack()
+llenarPila()
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 # Búsqueda recursiva de un elemento en una pila.
 def buscarPila(pila, elemento):
-    if len(pila) == 0:
+    if pila.size() == 0:
         print("Elemento no encontrado.")
         return
     temp = pila.pop()
     if temp == elemento:
         print("Elemento encontrado.")
-        pila.append(temp)
+        pila.push(temp)
     else:
         buscarPila(pila, elemento)
-        pila.append(temp)
+        pila.push(temp)
 
-# pila = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
 # buscarPila(pila, input("Buscar elemento: "))
 
+# distancia = buscarPila(pila, "c")
+# print(f"Si se quitan los últimos {distancia} elementos de la pila, se podrá acceder al elemento buscado.")
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Búsqueda recursiva de un elemento en una pila con distancia entre el último elemento y el buscado, o ubicación del elemento dentro de la pila.
 def buscarPilaDistancia(pila, elemento):
-    if len(pila) == 0:
+    if pila.size() == 0:
         print("Elemento no encontrado.")
         return -1
     temp = pila.pop()
     if temp == elemento:
         print("Elemento encontrado.")
-        pila.append(temp)
+        pila.push(temp)
         return 0
     else:
         distancia = buscarPilaDistancia(pila, elemento)
         if distancia != -1:
             distancia += 1
-        pila.append(temp)
+        pila.push(temp)
         return distancia
 
-# pila = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
-# distancia = buscarPila(pila, "c")
-# print(f"Si se quitan los últimos {distancia} elementos de la pila, se podrá acceder al elemento buscado.")
+# distancia = buscarPilaDistancia(pila, input("Buscar elemento: "))
+# if distancia != -1:
+#     print(f"Si se quitan los últimos {distancia} elementos de la pila, se podrá acceder al elemento buscado.")
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#
+def supImpares(pila):
+    if pila.size() == 0:
+        return
+    temp = pila.pop()
+    if temp % 2 == 0:
+        supImpares(pila)
+    else:
+        supImpares(pila)
+        pila.push(temp)
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Hola Mundo
 import time
@@ -82,7 +214,7 @@ def holaMundo(cadena):
 
 # holaMundo(0)
 
-
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def holaMundo_auto(cadena):
     if not cadena:
@@ -110,3 +242,6 @@ def holaMundo_auto(cadena):
     return frases_HM[9]
 
 # holaMundo_auto(0)
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
